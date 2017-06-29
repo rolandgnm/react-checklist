eb1009c38374a7fd16124a9da6d9d8a6ed403ce6
Roland Molina <rolandmolina@TopBettas-MacBook-Pro.local>
2017-06-29 16:02:19 +1000

    redux first run without warnings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Checklist from './container/Checklist';
+import Checklist from './containers/Checklist';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
+import {addItem} from './actions/ChecklistActions'
+import {store} from './store'
+
+
+console.log(store.getState())
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch(addItem('new item criado'))
+store.dispatch(addItem('new item criado'))
+store.dispatch(addItem('new item criado'))
+store.dispatch(addItem('new item criado'))
+store.dispatch(addItem('new item criado'))
+
+
 ReactDOM.render(<Checklist />, document.getElementById('root'));
 registerServiceWorker();
+
+unsubscribe()
