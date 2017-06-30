96d34a3febc45d88d859e4c48efae542f415d360
Roland Molina <rolandmolina@TopBettas-MacBook-Pro.local>
2017-06-30 11:52:02 +1000

    configures Redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,5 @@ import {createStore} from 'redux'
 import {checklist} from '../reducers'
 
 
-export let store = createStore(checklist)
+export let store = createStore(checklist, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
